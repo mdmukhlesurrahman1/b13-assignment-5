931c12e95b5306040fc964982b9ac4bca28bdcd9
mdmukhlesurrahman1 <mdmukhlesurrahman1@gmail.com>
2026-03-07 21:45:50 +0600

    Login Page Working...

---
+++ js/login.js
@@ -0,0 +1,19 @@
+const loginBtn = document.getElementById("login-btn");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+
+loginBtn.addEventListener("click", function () {
+    if (username.value === "admin" && password.value === "admin123") {
+        window.location.href="main.html";
+        alert("Login Successfully!");
+    }
+    else if (username.value !== "admin" && password.value !== "admin123") {
+        alert("Incorrect Username and Password!");
+    }
+    else if (password.value === "admin123") {
+        alert("Incorrect Username!");
+    }
+    else{
+        alert("Incorrect Password!");
+    }
+});
