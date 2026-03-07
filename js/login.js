0793f3897e8e05f978596e22347066ae2b3e4927
mdmukhlesurrahman1 <mdmukhlesurrahman1@gmail.com>
2026-03-08 00:30:24 +0600

    design uploading...

--- js/login.js
+++ js/login.js
@@ -3,17 +3,21 @@ const username = document.getElementById("username");
 const password = document.getElementById("password");
 
 loginBtn.addEventListener("click", function () {
-    if (username.value === "admin" && password.value === "admin123") {
-        window.location.href="main.html";
-        alert("Login Successfully!");
+    if (username.value === "" || password.value === "") {
+        alert("Fill Username and Password!");
     }
-    else if (username.value !== "admin" && password.value !== "admin123") {
-        alert("Incorrect Username and Password!");
-    }
-    else if (password.value === "admin123") {
-        alert("Incorrect Username!");
-    }
-    else{
-        alert("Incorrect Password!");
+    else {
+        if (username.value === "admin" && password.value === "admin123") {
+            window.location.href = "main.html";
+        }
+        else if (username.value !== "admin" && password.value !== "admin123") {
+            alert("Incorrect Username and Password!");
+        }
+        else if (password.value === "admin123") {
+            alert("Incorrect Username!");
+        }
+        else {
+            alert("Incorrect Password!");
+        }
     }
 });
